4400cfaf5d3e8a99f2acae6649569f97eb1c72ab
Chenkehan21 <56097561+Chenkehan21@users.noreply.github.com>
2020-01-27 23:34:45 +0800

    Add files via upload

---
+++ 小组作业三.py
@@ -0,0 +1,24 @@
+# 字符串拼接
+# 通过 % 将 name, age, code 拼接成一句话
+# 输出 Crossin is 18, he writes Python.
+name = 'Crossin'
+age = 18
+code = 'Python'
+print("%s is %d, he writes %s" % (name, age, code))
+
+# 类型转换
+num1 = '3.3'
+num2 = 2.5
+num1 = float(num1)
+print(num1 + num2)
+
+# bool
+print(bool(-123))
+print(bool(0)) # pay attention!
+print(bool('abc'))
+print(bool('False'))
+print(bool(''))
+print(bool([]))
+print(bool({}))
+print(bool(['']))
+print(bool(None))
---
+++ 小组作业二.py
@@ -0,0 +1,22 @@
+# 1、输出变量让用户猜一个数字并打印出来
+num = input("Please guess a number: ")
+print("You guessed: ", num)
+
+# 2、变量计算
+money = 20
+result = money * 2 + 50
+print(result)
+
+# 3、变量类型
+a = 'hello,world'
+b = 1.414
+c = (1 != 2)
+d = (True and False)
+e = (True or False)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print('\n')
+print(a, b, c, d, e, sep='\n') # better !!
